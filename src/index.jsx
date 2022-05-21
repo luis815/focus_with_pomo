ae49b847beae80ea3685b75d9fb65c3a91591eb7
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-05-21 01:06:58 +0000

    Vite react setup

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<h1>Hello from react!</h1>
+	</StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 const { resolve } = require("path");
 const { defineConfig } = require("vite");
+import react from "@vitejs/plugin-react";
 
 module.exports = defineConfig({
 	root: resolve(__dirname, "./src/"),
+	plugins: [react()],
 	publicDir: resolve(__dirname, "./public/"),
 	server: {
 		host: "0.0.0.0",
