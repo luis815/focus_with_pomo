92a4f9119f976e850ab25fb5e333e9439627eeb7
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-05-21 01:18:51 +0000

    Normalize.css

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,5 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<h1>Hello from react!</h1>
-	</StrictMode>
+	<StrictMode></StrictMode>
 );
