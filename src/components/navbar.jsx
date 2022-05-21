0f88b08efb691216e802ad6c5abe3d8ecc0ea402
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-05-21 11:30:08 +0000

    Navbar markup

---
+++ src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default () => {
+	return (
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>Settings</li>
+			</ul>
+			<ul>
+				<li>
+					<button type="button">Sign In</button>
+				</li>
+				<li>
+					<button type="button">Dark Mode</button>
+				</li>
+				<li>
+					<button type="button">Buy Me a Coffee</button>
+				</li>
+			</ul>
+		</nav>
+	);
+};
