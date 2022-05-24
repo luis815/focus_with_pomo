bbc7d1f52bd14d87f39f5d8cf65997fd7c414f7c
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-05-24 03:35:38 +0000

    React router

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,18 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./views/home.jsx";
+import Settings from "./views/settings.jsx";
+import PageNotFound from "./views/page-not-found.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<StrictMode></StrictMode>
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="settings" element={<Settings />} />
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
 );
---
+++ src/views/home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Fragment } from "react";
+
+export default () => {
+	return <Fragment></Fragment>;
+};
