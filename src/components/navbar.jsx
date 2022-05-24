5df04c76be4bea5d8453c21c8515ac6b00bf7673
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-05-24 18:45:16 +0000

    Mobile navigation burger

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
+
+import "../scss/navbar.scss";
 
 export default () => {
+	const [isHamburgerActive, setIsHamburgerActive] = useState(false);
+
+	const toggleHamburger = () => {
+		setIsHamburgerActive(!isHamburgerActive);
+	};
+
 	return (
 		<nav>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/settings">Settings</Link>
-				</li>
-			</ul>
-			<ul>
-				<li>
-					<button type="button">Sign In</button>
-				</li>
-				<li>
-					<button type="button">Dark Mode</button>
-				</li>
-				<li>
-					<button type="button">Buy Me a Coffee</button>
-				</li>
-			</ul>
+			<div className="brand">
+				<img src="/focus-with-pomo.svg" alt="Focus with Pomo logo" />
+				<div>
+					<span>Focus</span> <span>with</span> <span>Pomo</span>
+				</div>
+			</div>
+			<button
+				className={classNames({ hamburger: true, active: isHamburgerActive })}
+				onClick={toggleHamburger}
+			>
+				<span />
+			</button>
+			<div className="directory">
+				<ul className="views">
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/settings">Settings</Link>
+					</li>
+				</ul>
+				<ul className="actions">
+					<li>
+						<button type="button">Sign In</button>
+					</li>
+					<li>
+						<button type="button">Dark Mode</button>
+					</li>
+					<li>
+						<button type="button">Buy Me a Coffee</button>
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 };
