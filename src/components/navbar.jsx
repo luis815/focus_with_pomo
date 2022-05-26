373ad50eff960265fedebcdc9d97fdbe9e4af034
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-05-26 11:25:45 +0000

    Navbar reset

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,41 +11,5 @@ export default () => {
 		setIsHamburgerActive(!isHamburgerActive);
 	};
 
-	return (
-		<nav>
-			<div className="brand">
-				<img src="/focus-with-pomo.svg" alt="Focus with Pomo logo" />
-				<div>
-					<span>Focus</span> <span>with</span> <span>Pomo</span>
-				</div>
-			</div>
-			<button
-				className={classNames({ hamburger: true, active: isHamburgerActive })}
-				onClick={toggleHamburger}
-			>
-				<span />
-			</button>
-			<div className="directory">
-				<ul className="views">
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/settings">Settings</Link>
-					</li>
-				</ul>
-				<ul className="actions">
-					<li>
-						<button type="button">Sign In</button>
-					</li>
-					<li>
-						<button type="button">Dark Mode</button>
-					</li>
-					<li>
-						<button type="button">Buy Me a Coffee</button>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
+	return <nav></nav>;
 };
