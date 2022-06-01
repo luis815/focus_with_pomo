ef6760211caf11dae69d797f75eeb1cd6490e973
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-06-01 12:34:03 +0000

    Moved navbar styles to components.scss

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
 
-import "../styles/navbar.scss";
-
 export default () => {
 	const [isHamburgerActive, setIsHamburgerActive] = useState(false);
 
