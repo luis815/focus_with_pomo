57395d93c1bc480162eb24ca52af264c97c3114a
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-06-01 16:48:06 +0000

    Added custom user home hero

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,20 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext, Fragment } from "react";
 
 import ProgressRing from "../components/progress-ring.jsx";
 import RepCounter from "../components/rep-counter.jsx";
 import Reminders from "../components/reminders.jsx";
+import { UserContext } from "../components/context.jsx";
 
 export default () => {
 	const [focusList, setFocusList] = useState([]);
 	const [backlog, setBacklog] = useState([]);
 
-	useEffect(() => {
+	const [user] = useContext(UserContext);
 
-	}, []);
+	useEffect(() => {}, []);
 	return (
 		<div className="home">
 			<div className="hero">
-				<h1>Hello!</h1>
+				{user === null ? (
+					<h1>Hello!</h1>
+				) : (
+					<Fragment>
+						<h2>Welcome back,</h2>
+						<h1>Luis Lopez Cardona</h1>
+					</Fragment>
+				)}
 			</div>
 			<ProgressRing percent={75} time="15:00" />
 			<RepCounter total={4} current={1} />
