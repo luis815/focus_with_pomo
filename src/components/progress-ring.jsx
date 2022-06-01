fb9b44f4900944f38008746dc8a9dc0582e5a84d
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-06-01 12:08:00 +0000

    Refactored progress ring

--- src/components/progress-ring.jsx
+++ src/components/progress-ring.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default ({ percent, time }) => {
 	return (
-		<div className="progress-container">
+		<div className="progress-ring">
 			<svg viewBox="-5 -5 110 110" xmlns="http://www.w3.org/2000/svg">
 				<circle cx="50" cy="50" r="50" />
 				<circle
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 
-import ProgressBar from "../components/progress-bar.jsx";
+import ProgressRing from "../components/progress-ring.jsx";
 import RepCounter from "../components/rep-counter.jsx";
 import TodoList from "../components/todo-list.jsx";
 
@@ -12,10 +12,8 @@ export default () => {
 			<div className="hero">
 				<h1>Hello!</h1>
 			</div>
-			<div id="timer">
-				<ProgressBar percent={75} time="15:00" />
-				<RepCounter total={4} current={1} />
-			</div>
+			<ProgressRing percent={75} time="15:00" />
+			<RepCounter total={4} current={1} />
 			<div id="reminders">
 				<TodoList heading={"Focus"} />
 				<TodoList heading={"To Do"} />
