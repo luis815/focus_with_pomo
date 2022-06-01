1d48cbc23276156aa453a0fec30c4988bbf3bfa7
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-06-01 15:21:52 +0000

    Implemented basic reminder functionality

--- src/components/reminders.jsx
+++ src/components/reminders.jsx
@@ -1,27 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default ({ heading, list, setList }) => {
+	const [newItem, setNewItem] = useState("");
+
+	const handleNewItem = () => {
+		if (newItem.length === 0) {
+			return;
+		}
+
+		setList([...list, newItem]);
+		setNewItem("");
+	};
+
+	const handleDeleteItem = (index) => {
+		const tempList = [...list];
+		tempList.splice(index, 1);
+		setList(tempList);
+	};
+
 	return (
 		<div className="reminders">
 			<h2>{heading}</h2>
 			<div className="list">
+				{list.map((item, index) => (
+					<div className="item-container" key={index}>
+						<input
+							type="text"
+							autoCapitalize="true"
+							autoComplete="true"
+							autoCorrect="true"
+							value={item}
+							disabled
+						/>
+						<button type="button" onClick={() => handleDeleteItem(index)} />
+					</div>
+				))}
 				<div className="item-container">
 					<input
 						type="text"
 						autoCapitalize="true"
 						autoComplete="true"
 						autoCorrect="true"
+						value={newItem}
+						onChange={(e) => setNewItem(e.target.value)}
+						onKeyDown={(e) => (e.key === "Enter" ? handleNewItem() : null)}
 					/>
-					<button>+</button>
-				</div>
-				<div className="item-container">
-					<input
-						type="text"
-						autoCapitalize="true"
-						autoComplete="true"
-						autoCorrect="true"
+					<button
+						className="new"
+						type="button"
+						onClick={handleNewItem}
 					/>
-					<button className="new">+</button>
 				</div>
 			</div>
 		</div>
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,10 +1,16 @@
-import React, { Fragment } from "react";
+import React, { useState, useEffect } from "react";
 
 import ProgressRing from "../components/progress-ring.jsx";
 import RepCounter from "../components/rep-counter.jsx";
 import Reminders from "../components/reminders.jsx";
 
 export default () => {
+	const [focusList, setFocusList] = useState([]);
+	const [backlog, setBacklog] = useState([]);
+
+	useEffect(() => {
+
+	}, []);
 	return (
 		<div className="home">
 			<div className="hero">
@@ -12,8 +18,8 @@ export default () => {
 			</div>
 			<ProgressRing percent={75} time="15:00" />
 			<RepCounter total={4} current={1} />
-			<Reminders heading={"Focus"} />
-			<Reminders heading={"To Do"} />
+			<Reminders heading={"Focus"} list={focusList} setList={setFocusList} />
+			<Reminders heading={"Backlog"} list={backlog} setList={setBacklog} />
 		</div>
 	);
 };
