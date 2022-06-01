bbf85a02e5cf2e6ce21583b35bdf975ebeb8a190
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-06-01 15:41:35 +0000

    Initialized firebase firestore

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firestore } from "firebase/firestore";
 
 import DefaultLayout from "./components/default-layout.jsx";
 
@@ -9,6 +12,19 @@ import SettingsView from "./views/settings.jsx";
 import PageNotFoundView from "./views/page-not-found.jsx";
 
 import "./styles/base.scss";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDtvE0ai_iUbwtC29AN1RU36qxzY1Cf3O4",
+	authDomain: "focus-with-pomo.firebaseapp.com",
+	projectId: "focus-with-pomo",
+	storageBucket: "focus-with-pomo.appspot.com",
+	messagingSenderId: "430422384839",
+	appId: "1:430422384839:web:2b397135a2272315f75ee2",
+	measurementId: "G-2YXKH6XLG6",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<StrictMode>
